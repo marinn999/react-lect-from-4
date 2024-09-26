023f2a4b6386470c57e278203dd19c9149303c35
marinn999 <marinacompel@icloud.com>
2024-09-26 16:06:50 +0200

    load_more

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ArticlesList from "./components/ArticlesList/ArticlesList";
 import Loader from "./components/Loader/Loader";
 
 function App() {
-  //Це це я зроблений методом аксіос. Але ми вчимо інший метод, тому коментую
+  //Це function зробленa методом аксіос. Але ми вчимо інший метод, тому коментую
   // useEffect(() => {
   //   axios
   //     .get("https://hn.algolia.com/api/v1/search?query=react")
@@ -15,6 +15,8 @@ function App() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(0);
+  // const [totalPages, setTotalPages] = useState(0);
 
   //   Читаю з права наліво:
   // Даю запит на сервер
@@ -28,12 +30,12 @@ function App() {
         // 1. Зробили завантаження
         setIsLoading(true);
         // 2. Прийшли дані
-        const data = await fetchArticles();
+        const data = await fetchArticles(page);
         // 3. Скидаємо завантаження, але в цьому блоці не пишемо
         // setIsLoading(false);
         //Кидаю його в блок finally, який спрацьовує і в try, і в catch
         //4.Записую дані в стейт
-        setArticles(data.hits);
+        setArticles((prev) => [...prev, ...data.hits]);
         //Щоб точно не показувалась помилка, де її немає, бо з новим запитом має зникнути все старе
         setIsError(false);
       } catch {
@@ -44,33 +46,25 @@ function App() {
       }
     };
     getData();
-  }, []);
+  }, [page]);
+
+  const handleChangePage = () => {
+    setPage((prev) => prev + 1);
+  };
+
   return (
     <div>
       <h2>HTTP</h2>
-      {/* ................................................... */}
-      {/* <ArticlesList articles={articles} />
-      Коли даних немає, то не має нічого рендеритись, а ul рендериться. Коли ми
-      робимо фетч, то ul відмальовується, хоч і пустий. Треба зробити так, щоб
-      він не малювався, якщо немає даних */}
-      {/* ................................................... */}
-      {/* {articles.length && <ArticlesList articles={articles} />}
-      Якщо article.length існує, тоді малюй список. Але при повільному інтернеті
-      промальовується нуль (0), бо спочатку article.length=0, тоді логічне «і»
-      (&&) переривається і промальовується лише нуль */}
-      {/* .........................1.......................... */}
-      {/* Перше рішення: використання тернарного оператора:
-      «Якщо даних нема (articles.length=0), то показуй нічого (null)
-      {articles.length ? <ArticlesList articles={articles} /> : null} */}
-      {/* .........................2.......................... */}
-      {/* Друге рішення: це приведення до булевого формату (!!)
-      {!!articles.length && <ArticlesList articles={articles} />} */}
-      {/* .........................3.......................... */}
-      {/* Або максимально просто */}
+
+      {/* «Якщо даних нема (articles.length=0), то показуй нічого (null) */}
       {articles.length > 0 && <ArticlesList articles={articles} />}
 
       {isLoading && <Loader />}
       {isError && <h2>Smth is wrong. ERROR</h2>}
+      {/* {totalPages > page && (
+        <button onClick={handleChangePage}> Load More</button>
+      )} */}
+      <button onClick={handleChangePage}> Load More</button>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 // Функція що робить запит на сервер і повертає відповідь.
 
-export const fetchArticles = async () => {
+export const fetchArticles = async (page = 0) => {
   const { data } = await axios.get(
-    "https://hn.algolia.com/api/v1/search?query=react"
+    `https://hn.algolia.com/api/v1/search?query=react&page=${page}`
   );
   return data;
 };
